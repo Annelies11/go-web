621fcaeb79ea85562ad6c60863f9f06e70bda59d
Annelies11 <mmahmudi1103@gmail.com>
2024-04-07 20:57:06 +0700

    first commit

---
+++ handler_test.go
@@ -0,0 +1,23 @@
+package goweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>Hello Brooo</h1>")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
